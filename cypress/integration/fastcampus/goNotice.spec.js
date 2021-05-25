985352746bf7ea49775404f9d2cf2aa0976c914b
hyoniiday <47749336+hyoniiday@users.noreply.github.com>
2021-05-25 15:21:14 +0900

    공지사항 페이지 검증 추가

--- cypress/integration/fastcampus/goNotice.spec.js
+++ cypress/integration/fastcampus/goNotice.spec.js
@@ -6,7 +6,9 @@ describe('공지사항 이동', function () {
         cy.scrollTo('bottom')
         cy.wait(2000)
         cy.contains('공지사항').click()
-
+    
+        //공지사항 페이지인지 확인 should
+        cy.get('h2.vn-section__title').should('contain','공지사항')
     })
 
 })
