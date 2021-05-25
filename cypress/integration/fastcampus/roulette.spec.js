71d66fb66e85d313450307792d3a21810b8caef5
hyoniiday <47749336+hyoniiday@users.noreply.github.com>
2021-05-25 14:29:00 +0900

    룰렛참여기능추가

--- cypress/integration/fastcampus/roulette.spec.js
+++ cypress/integration/fastcampus/roulette.spec.js
@@ -1,10 +1,10 @@
 describe('룰렛 이벤트 참여하기', function () {
-    /*
+/*
         it('정상적인 아이디로 로그인', function () {
             // 사이트 접속
             // 환경과 서비스명을 이용해서 사이트 접속하도록 구현 필요
     
-            cy.visit('/')
+            cy.visit('https://fastcampus.co.kr/event_online_roulette')
     
             //로그인 페이지로 이동하는 PATH
             cy.contains('로그인').click()
@@ -13,46 +13,48 @@ describe('룰렛 이벤트 참여하기', function () {
             // 이메일을 입력한다. 
             // DB에서 가져오는 구조로 구현 필요
             cy.get('input[name=email]')
-                .type('jinhwan.choi@fastcampus.co.kr', {
+                .type('jihyeon.lee@fastcampus.co.kr', {
                     delay: 100
                 })
-                .should('have.value', 'jinhwan.choi@fastcampus.co.kr')
+                .should('have.value', 'jihyeon.lee@fastcampus.co.kr')
     
             // 비밀번호를 입력한다.
             // DB에서 가져오는 구조로 구현 필요
             cy.get('input[name=password]')
-                .type('test1234')
+                .type('test1234!')
                 
             //로그인 버튼을 누른다.
             cy.get('.btn').click()
         })
-    */
+*/
         it('룰렛이벤트 이동', function () {
-            cy.visit('/')
-            //cy.get('#nav-flyout-ya-signin').invoke('show');
-            //click on the hidden element
-            cy.wait(2000)
-            cy.get('#nav > div:nth-child(5) > div > a.nav-banner.nav-banner').click({force: true})
-          //  cy.contains('수강 신청하기').click()
+            //cy.visit('/')
+            //cy.wait(2000)
+            //cy.get('#nav > div:nth-child(5) > div > a.nav-banner.nav-banner').click({force: true})
+            cy.visit('https://fastcampus.co.kr/event_online_roulette')
+            cy.get('.spin-button__background').click({force: true})
+                        //cy.scrollTo('bottom')
         })
-    /*
+    
+
         it('한번 더 로그인', function () {
         cy.contains('이메일로 시작하기').click()
     
         // 이메일을 입력한다. 
         // DB에서 가져오는 구조로 구현 필요
         cy.get('input[name=email]')
-            .type('jinhwan.choi@fastcampus.co.kr', { delay: 100 })
-            .should('have.value', 'jinhwan.choi@fastcampus.co.kr')
+            .type('jihyeon.lee@fastcampus.co.kr', { delay: 10 })
+            .should('have.value', 'jihyeon.lee@fastcampus.co.kr')
     
         // 비밀번호를 입력한다.
         // DB에서 가져오는 구조로 구현 필요
         cy.get('input[name=password]')
-            .type('test1234')
+            .type('test1234!')
     
         //로그인 버튼을 누른다.
         cy.get('.btn').click()
+
+        //룰렛 참여하기
+        cy.get('.spin-button__background').click({force: true})
         })
-    
-    */
     })
